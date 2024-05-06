6c356a515ac04980900e74e8b265eea89292561f
Felix Henninger <mailbox@felixhenninger.com>
2024-05-06 19:32:14 +0200

    Initial commit: Project dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Handbook dev environment";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            # Website
+            nodejs-18_x
+            nodePackages.yarn
+            zola
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
