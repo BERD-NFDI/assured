6577418f073101cf5d539f8f2702fb466aaa5898
Felix Henninger <mailbox@felixhenninger.com>
2024-05-11 23:03:31 +0200

    Add generic page structure

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontSize: {
+      sm: '1.1rem',
+      base: '1.4rem',
+      lg: '1.75rem',
+      xl: '2rem',
+      '2xl': '4rem',
+      '3xl': '6rem',
+    },
+    extend: {
+      colors: {
+        accent: '#459f9d',
+      },
+    },
+  },
+}
