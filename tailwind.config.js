0651f7c1a54a2016b99a14f77bef941a2dde1da8
Felix Henninger <mailbox@felixhenninger.com>
2024-05-21 14:06:58 +0200

    Include and apply Krub font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   theme: {
     fontSize: {
       sm: '1.1rem',
-      base: '1.4rem',
+      base: '1.25rem',
       lg: '1.75rem',
       xl: '2rem',
       '2xl': '4rem',
       '3xl': '6rem',
     },
     extend: {
+      fontFamily: {
+        sans: ['"Krub"'],
+      },
       colors: {
         accent: '#459f9d',
       },
